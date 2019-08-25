68d213bd483ad6eb21cd163f531fd065c615113c
bandidom9 <jrbandido@hotmail.com>
2019-08-25 11:17:10 -0400

    Update light-alarm.groovy

--- smartapps/bandidom9/light-alarm.src/light-alarm.groovy
+++ smartapps/bandidom9/light-alarm.src/light-alarm.groovy
@@ -10,7 +10,6 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Sonoff (Connect)
  *
  *  Author: Jose Perez (bandidom9)
  *  Date: 2018-09-09
